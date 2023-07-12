159c8fd6381d5d2855577067c6aa0a6541415979
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2023-07-12 17:37:01 -0300

    Adicionando condição no Exercicio 1

--- Exercicio_01/Program.cs
+++ Exercicio_01/Program.cs
@@ -3,6 +3,14 @@ int valorA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Digite o valor B : ");
 int valorB = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Valor A : " + valorB);
+if (valorA > 0)
+{
+    Console.Write("Valor A : " + valorB);
+}
+
 Console.Write("\n=============================================================\n");
-Console.Write("Valor B : " + valorA);
+
+if (valorB > 0)
+{
+    Console.Write("Valor B : " + valorA);
+}
