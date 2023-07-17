b0cc2aeb8bee5bbb88eb020840fd06b988c5f762
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2023-07-17 09:51:56 -0300

    feat : Resolvendo exercicio 3.

--- Exercicio_03/Program.cs
+++ Exercicio_03/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine(" Digite o 1º número :");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Digite o 2º número :");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Digite o 3º número :");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new[] { n1, n2, n3 };
+
+foreach (int value in arr)
+{
+    Console.Write(value + " ");
+}
