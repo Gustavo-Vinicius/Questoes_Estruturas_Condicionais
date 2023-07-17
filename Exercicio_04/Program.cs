2b07ecbc68db7e58e91f36df37a88e0522b23ad1
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2023-07-17 10:23:52 -0300

    feat : Resolução do exercicio 4.

---
+++ Exercicio_04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite o 1º número :");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o 2º número :");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2){
+    Console.Write($"Numero {n1} é maior que o {n2}");
+}
+else if (n2>n1) {
+    Console.Write($"O numero {n2} e maior do que {n1}");
+}
+else {
+    Console.Write("Numeros não informados");
+}
