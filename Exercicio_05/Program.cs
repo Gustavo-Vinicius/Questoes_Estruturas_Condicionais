8328d9c89dfd50e7bc3bb56ecd239d4600688d15
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2023-07-17 17:27:15 -0300

    feat : Resolendo exercicio 5

---
+++ Exercicio_05/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Digite a primeira nota :");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a segunda nota :");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota :");
+double nota3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a quarta nota :");
+double nota4 = Convert.ToDouble(Console.ReadLine());
+
+double resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+
+if (resultado >= 7)
+{
+    Console.Write("Aprovado com Distinção!");
+}
+else if (resultado < 7)
+{
+    Console.Write("Reprovado. \n");
+}
+
+if (resultado < 7)
+{
+    Console.WriteLine("Digite a nota da recuperacao :");
+    double recuperacao = Convert.ToDouble(Console.ReadLine());
+
+    double novoResultado = (resultado + recuperacao) / 2;
+
+    if (novoResultado >= 7)
+    {
+        Console.Write("Aprovado na recuperação !");
+    }
+    else if (novoResultado < 7)
+    {
+        Console.Write("Reprovado, mesmo com a nota da recuperação !");
+    }
+}
