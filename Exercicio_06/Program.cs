ebc95ac935d09269b598b3574e2c04127ddb2c85
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-22 10:40:36 -0300

    feat : Realizando criação do exercicio 6.

---
+++ Exercicio_06/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Digite o primeiro número :");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo número :");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2) {
+    Console.Write($"N1: {n1} é maior número.\n");
+    Console.Write($"N2: {n2} é menor número.");
+ }
+ else if (n2 > n1) {
+    Console.Write($"N2: {n2} é maior número.\n");
+    Console.Write($"N1: {n1} é menor número.");
+ } 
+ else if (n1 == n2) {
+    Console.Write($"Número 1 {n1} é igual a Número 2 {n2}");
+ } 
+else {
+    Console.WriteLine("Nenhum número foi informado");
+}
