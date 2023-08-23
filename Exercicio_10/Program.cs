7cd625c40492eda60278aa179378f8f4f2f5adba
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-23 14:09:09 -0300

    feat : Resolvendo problema 10 do exercicio.

---
+++ Exercicio_10/Program.cs
@@ -0,0 +1,27 @@
+﻿int a,
+    b,
+    c;
+
+do
+{
+    Console.Write("Digite um valor maior que zero para 'a': ");
+} while (!int.TryParse(Console.ReadLine(), out a) || a <= 0);
+
+do
+{
+    Console.Write("Digite um valor maior que zero para 'b': ");
+} while (!int.TryParse(Console.ReadLine(), out b) || b <= 0);
+
+do
+{
+    Console.Write("Digite um valor maior que zero para 'c': ");
+} while (!int.TryParse(Console.ReadLine(), out c) || c <= 0);
+
+int menor = Math.Min(Math.Min(a, b), c);
+int maior = Math.Max(Math.Max(a, b), c);
+
+int resultadoMultiplicacao = menor * maior;
+double resultadoDivisao = (double)maior / menor;
+
+Console.WriteLine($"Menor valor multiplicado pelo maior: {resultadoMultiplicacao}");
+Console.WriteLine($"Maior valor dividido pelo menor: {resultadoDivisao}");
