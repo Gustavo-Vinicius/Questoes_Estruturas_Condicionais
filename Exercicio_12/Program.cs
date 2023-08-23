c88a20bb08496b1cb1dcbf561bbdc530407718b1
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-23 17:38:31 -0300

    feat : Resolvendo problema 12 do exercicio.

---
+++ Exercicio_12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int a = 0; // Variável para armazenar o número positivo
+int b = 0; // Variável para armazenar o número negativo
+
+if (numero > 0)
+{
+    a = numero;
+}
+else if (numero < 0)
+{
+    b = numero;
+}
+
+Console.WriteLine($"Número positivo em A: {a}");
+Console.WriteLine($"Número negativo em B: {b}");
