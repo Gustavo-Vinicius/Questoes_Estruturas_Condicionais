ef828ef614d27fba96ff3ab85356711f5b7df17e
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-23 11:14:50 -0300

    feat :  Resolvendo problema 9 do exercicio.

---
+++ Exercicio_09/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Digite o valor A :");
+int valorA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n Digite o valor B:");
+int valorB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o valor C :");
+int valorC = Convert.ToInt32(Console.ReadLine());
+
+if (valorA + valorB > valorC && valorA + valorC > valorB && valorB + valorC > valorA)
+{
+    Console.WriteLine("Forma um Triângulo.");
+}
+else if(valorA + valorB == valorC && valorA + valorC == valorB && valorB + valorC == valorA){
+    Console.WriteLine("Forma um triângulo Equilátero.");
+}
+else if(valorA + valorB == valorC && valorA + valorC == valorB && valorB + valorC != valorA){
+    Console.WriteLine("Forma um triângulo Isóscelos.");
+}
+else if(valorA + valorB != valorC && valorA + valorC != valorB && valorB + valorC != valorA){
+    Console.WriteLine("Forma um triângulo Escaleno.");
+}
+
+
