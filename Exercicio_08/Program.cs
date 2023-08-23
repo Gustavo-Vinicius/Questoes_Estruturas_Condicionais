6b8a097b4af5ae137437ca53568bae99366631e7
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-23 08:52:20 -0300

    feat : Criando Exercicio 8.

---
+++ Exercicio_08/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("DIGITE UM NUMERO INTEIRO :");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+switch (numero)
+{
+    case 1:
+    Console.Write("Um");
+    break;
+    
+    case 2:
+    Console.Write("Dois");
+    break;
+
+    case 3:
+    Console.Write("Três");
+    break;
+
+    default:
+    Console.WriteLine("Nenhum número foi digitado.");
+    break;
+}
